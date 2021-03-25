0daf1a0a3f8f9e5503db4def0f88256acbec743d
amraa <amarbayarbatbayar2@gmail.com>
2021-03-25 10:46:12 +0800

    My project is finished

--- src/js/model/Search.js
+++ src/js/model/Search.js
@@ -1,19 +1,22 @@
 require("@babel/polyfill");
 import axios from "axios";
+
 export default class Search {
   constructor(query) {
     this.query = query;
   }
-  async doSearch(query) {
+
+  async doSearch() {
     try {
       let result = await axios(
         "https://forkify-api.herokuapp.com/api/search?q=" + this.query
       );
+
       this.result = result.data.recipes;
 
       return this.result;
     } catch (error) {
-      alert("ÐÑÑƒÑƒÐ´Ð°Ð» Ð³Ð°Ñ€Ð»Ð°Ð° : " + error);
+      console.log("Асуудал гарлаа : " + error);
     }
   }
 }
